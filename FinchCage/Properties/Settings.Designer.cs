a8471fe487f31af8c9cece90422eb79c96d68903
encino <encio.holanda@gmail.com>
2017-07-17 16:10:28 -0700

    Adding directBlock setting

--- FinchCage/Properties/Settings.Designer.cs
+++ FinchCage/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FinchCage.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,18 @@ namespace FinchCage.Properties {
             }
             set {
                 this["TargetDir"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("NZDCHF,EURNZD,I|AUDCHF,EURAUD,I")]
+        public string DirectBlock {
+            get {
+                return ((string)(this["DirectBlock"]));
+            }
+            set {
+                this["DirectBlock"] = value;
             }
         }
     }
